464382346125e166d2af07e3ed0fee51f71ae653
yangbin <1250211588@qq.com>
2020-03-23 17:14:58 +0800

    build

---
+++ src/com/java/Class1288.java
@@ -0,0 +1,32 @@
+package com.java;
+
+/**
+ * @author YangBin
+ * @date 2020年03月23日
+ * 1288. 删除被覆盖区间
+ * 给你一个区间列表，请你删除列表中被其他区间所覆盖的区间。
+ * <p>
+ * 只有当 c <= a 且 b <= d 时，我们才认为区间 [a,b) 被区间 [c,d) 覆盖。
+ * <p>
+ * 在完成所有删除操作后，请你返回列表中剩余区间的数目。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：intervals = [[1,4],[3,6],[2,8]]
+ * 输出：2
+ * 解释：区间 [3,6] 被区间 [2,8] 覆盖，所以它被删除了。
+ * <p>
+ * <p>
+ * 提示：​​​​​​
+ * <p>
+ * 1 <= intervals.length <= 1000
+ * 0 <= intervals[i][0] < intervals[i][1] <= 10^5
+ * 对于所有的 i != j：intervals[i] != intervals[j]
+ */
+public class Class1288 {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
