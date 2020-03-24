dd1715e9e72ef22fb98066972dd0d73cef956fbc
yangbin <1250211588@qq.com>
2020-03-24 11:00:00 +0800

    Class1289

--- src/main/java/com/java/Class1288.java
+++ src/main/java/com/java/Class1288.java
@@ -1,32 +1,59 @@
 package com.java;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author YangBin
  * @date 2020年03月23日
+ * https://leetcode-cn.com/problems/remove-covered-intervals/
  * 1288. 删除被覆盖区间
  * 给你一个区间列表，请你删除列表中被其他区间所覆盖的区间。
- * <p>
  * 只有当 c <= a 且 b <= d 时，我们才认为区间 [a,b) 被区间 [c,d) 覆盖。
- * <p>
  * 在完成所有删除操作后，请你返回列表中剩余区间的数目。
- * <p>
- * <p>
- * <p>
  * 示例：
- * <p>
  * 输入：intervals = [[1,4],[3,6],[2,8]]
  * 输出：2
  * 解释：区间 [3,6] 被区间 [2,8] 覆盖，所以它被删除了。
- * <p>
- * <p>
  * 提示：​​​​​​
- * <p>
  * 1 <= intervals.length <= 1000
  * 0 <= intervals[i][0] < intervals[i][1] <= 10^5
  * 对于所有的 i != j：intervals[i] != intervals[j]
  */
 public class Class1288 {
+
     public static void main(String[] args) {
-        System.out.println("1");
+        int[][] intervals = {{1, 4}, {3, 6}, {2, 8}};
+
+        System.out.println(removeCoveredIntervals(intervals));
+    }
+
+    public static int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o2[1] - o1[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        int count = 0;
+        lable:
+        for (int i = 0; i < intervals.length; i++) {
+            int[] a = intervals[i];
+            for (int i1 = 0; i1 < intervals.length; i1++) {
+                int[] b = intervals[i1];
+                if (i != i1) {
+                    if (a[0] >= b[0] && a[1] <= b[1]) {
+                        continue lable;
+                    }
+                }
+            }
+            count++;
+        }
+
+        return count;
     }
 }
---
+++ src/main/java/com/java/Class1289.java
@@ -0,0 +1,64 @@
+
+package com.java;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author YangBin
+ * @date 2020年03月23日
+ * https://leetcode-cn.com/problems/minimum-falling-path-sum-ii/
+ * 1289. 下降路径最小和  II
+ * 给你一个整数方阵 arr ，定义「非零偏移下降路径」为：从 arr 数组中的每一行选择一个数字，且按顺序选出来的数字中，相邻数字不在原数组的同一列。
+ * <p>
+ * 请你返回非零偏移下降路径数字和的最小值。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：13
+ * 解释：
+ * 所有非零偏移下降路径包括：
+ * [1,5,9], [1,5,7], [1,6,7], [1,6,8],
+ * [2,4,8], [2,4,9], [2,6,7], [2,6,8],
+ * [3,4,8], [3,4,9], [3,5,7], [3,5,9]
+ * 下降路径中数字和最小的是 [1,5,7] ，所以答案是 13 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= arr.length == arr[i].length <= 200
+ * -99 <= arr[i][j] <= 99
+ * 通过次数1,335提交次数2,269
+ */
+public class Class1289 {
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        System.out.println(minFallingPathSum(arr));
+    }
+
+    public static int minFallingPathSum(int[][] arr) {
+        TreeSet<Integer> sumSet = new TreeSet<Integer>();
+        int num = arr.length;
+        int size = arr[0].length;
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int[] nums = new int[num];
+            int sum = 0;
+            for (int anInt : arr[i]) {
+                sum += anInt;
+            }
+            sumSet.add(sum);
+        }
+        return sumSet.first();
+    }
+
+}
