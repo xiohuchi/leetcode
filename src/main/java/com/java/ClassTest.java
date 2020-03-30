7d203a72dd003ae1388d5d1cb386dfadf3a7ec8f
yangbin <1250211588@qq.com>
2020-03-30 18:55:27 +0800

    ClassTest

---
+++ src/main/java/com/java/ClassTest.java
@@ -0,0 +1,19 @@
+
+package com.java;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ClassTest {
+
+    public static void main(String[] args) {
+        Map<Object, Object> map = new LinkedHashMap<>();
+        map.put("003", "1");
+        map.put("897", "1");
+        map.put("568", "2");
+        System.out.println(map);
+        System.out.println(4 & 2);
+    }
+
+
+}
---
+++ src/main/java/com/java/learn/LRedisson.java
@@ -0,0 +1,10 @@
+package com.java.learn;
+
+/**
+ * Redisson 的一些常见用例
+ *
+ * @author YangBin
+ * @date 2020年03月30日
+ */
+public class LRedisson {
+}
